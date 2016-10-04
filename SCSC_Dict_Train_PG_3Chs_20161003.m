6b9649d5a6448897b073d65a42a7b471b436d0e5
XuJun <nankaimathxujun@gmail.com>
2016-10-04 18:22:00 +0800

    MODIFY

---
+++ SCSC_Dict_Train_PG_3Chs_20161003.m
@@ -0,0 +1,41 @@
+% semi-coupled weighted sparse coding
+clear;
+addpath('Data');
+addpath('SPAMS'); 
+addpath('Utilities');
+
+load Data/params_gray_PG.mat;
+task = 'BID';
+
+for j = 1:par.Patch_Channel
+    modelname = sprintf('../DSCDL_BID/Data/GMM_PG_%d_10_8x8_64_20161003T094301.mat',j);
+    eval(['load ' modelname]);
+    D = cell(par.Patch_Channel,par.cls_num);
+    W = cell(par.Patch_Channel,par.cls_num);
+    for i = 1 : par.cls_num
+        XN_t = double(Xn{j,i});
+        XC_t = double(Xc{j,i});
+        fprintf('SCSC: Channel: %d, Cluster: %d\n', j, i);
+        % clean
+        [GMM_Dc,GMM_Sc,~] = svd(model.covs(:,:,i));
+        GMM_Sc = diag(GMM_Sc);
+        % noisy
+        [GMM_Dn,GMM_Sn,~] = svd(XN_t*XN_t'/size(XN_t,2));
+        GMM_Sn = diag(GMM_Sn);
+        % paired dictionary and weights
+        SCSC.DC{j,i} = GMM_Dc;
+        SCSC.DN{j,i} = GMM_Dn;
+        SCSC.WC{j,i} = sqrt(GMM_Sc);
+        SCSC.WN{j,i} = sqrt(GMM_Sn);
+        % paired coefficient and mapping
+        Alphac = GMM_Dc'*XC_t;
+        Alphan = GMM_Dn'*XN_t;
+        [Alphac, Alphan, Uc, Un, Pn, f] = SCSC_DAP(Alphac, Alphan, XC_t, XN_t, GMM_Dc, GMM_Dn, par);
+        SCSC.UC{j,i} = Uc;
+        SCSC.UN{j,i} = Un;
+        SCSC.PN{j,i} = Pn;
+        SCSC.f{j,i} = f;
+        Dict_BID = sprintf('Data/SCSC_PG_3_10_8x8_64_%s_20161003.mat',task);
+        save(Dict_BID,'SCSC');
+    end
+end
